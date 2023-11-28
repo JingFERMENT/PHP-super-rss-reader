d8ad260b0b52565f219ce13a06649d9a6fc3d1b1
Kevin Lavenant <kevinlavenant@MacBook-Pro-de-Kevin.local>
2023-11-28 16:46:17 +0100

    squelette créé

---
+++ views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <!-- style -->
+    <link rel="stylesheet" href="../public/assets/css/style.css">
+</head>
+<body>
+    test
+
+</body>
+</html>
