c2dec073c85ffce5c0d2d27fe2a7ced756225e5b
NicolasDona <nicolas.donascimento@gmail.com>
2023-11-29 11:09:29 +0100

    extraction xml rss

--- controllers/pages-ctrl.php
+++ controllers/pages-ctrl.php
@@ -1,36 +1,25 @@
 <?php
 // L'URL du flux RSS à lire
 $url = 'https://www.lemonde.fr/europe/rss_full.xml';
-
 // Chargement du flux RSS avec SimpleXML
 // simplexml_load_file — Convertit un fichier XML en objet
 $rss = simplexml_load_file($url);
-
 // Parcours des éléments du flux
 foreach ($rss->channel->item as $item) {
     // Récupération du titre, du lien et de la description
     $title = $item->title;
     $link = $item->link;
     $description = $item->description;
-
-    // Affichage des informations
-    echo "<h2>$title</h2><br>";
-    echo $description."<br>";
-
-    // Gestion des erreurs, variable pour le tableau des erreurs?
+    // Gestion de la date
+    $dateItem = $item->pubDate;
+    $timestamp = strtotime($dateItem);
+    $formatter = new IntlDateFormatter('fr_FR');
+    $formatter->setPattern('EEEE dd MMMM YYYY H:mm');
+    $formattedDate = $formatter->format($timestamp);
     // Récupération et affichage de l'image (si présente)
     $namespaces = $item->getNameSpaces(true);
     $media = $item->children($namespaces['media']);
-    if (!empty($media->content)) {
-        $imageUrl = $media->content->attributes()->url;
-        //regex à revoir si il faut contrôler le type MIME
-        if (filter_var($imageUrl, FILTER_VALIDATE_URL) && preg_match('/\.(jpg|jpeg|png)$/', $imageUrl)) {
-            // Nettoyage de l'URL pour éviter les injections de code
-            $cleanImageUrl = htmlspecialchars($imageUrl, ENT_QUOTES, 'UTF-8');
-            echo "<img src='$cleanImageUrl' alt='Image'><br>";
-        } else {
-            echo "<p>Image non valide ou format non supporté.</p>";
-        }
-    }
+    $imageUrl = $media->content->attributes()->url;
+    // Affichage des informations Europe
+    // $title - $formattedDate - $description - $link - $imageUrl;
 }
-?>
