0960d1452b7ee2f8fb555243691983aceab78560
NicolasDona <nicolas.donascimento@gmail.com>
2023-12-01 10:02:07 +0100

    mentions

---
+++ controllers/mentions-ctrl.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../config/init.php';
+
+include __DIR__ . '/../views/templates/header.php';
+include __DIR__ . '/../views/mentions.php';
+include __DIR__ . '/../views/templates/footer.php';
