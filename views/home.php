6ab3d189a77c2a8005e32eb5c07c76d9a7e8f062
Jing ZHANG <jing.zhang.ferment@gmail.com>
2023-12-01 09:39:16 +0100

    ajout popup

--- views/home.php
+++ views/home.php
@@ -10,8 +10,20 @@
 </section>
 <section>
     <div class="container py-5">
+    <?php if ($popup) { ?>
+            <div class="alert alert-light alert-dismissible fade show popup shadow-lg" role="alert">
+            <div class="p-5 text-center">
+                <h3 class="fw-bold">Bienvenue sur votre espace RSS</h3>
+                <h3 class="fw-bold mb-4">CKJN global news</h3>
+                <h5 class="mb-4">Pour personnaliser les thèmes liés à l'actualité mondiale,<br> cliquez sur le bouton "Parametre"</h5>
+                <a class="btn btn-primary mb-4" href="/parametre.html">Paramètres<i class="fa-solid fa-gears text-light pe-0"></i></a>
+                <p class="fw-bold">Enregistrez jusqu'à 3 thèmes dans votre espace.</p>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="alert" aria-label="Close">Fermer</button>
+            </div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php } ?>
         <div class="row justify-content-center">
-
 
         <?php
             foreach ($urlsRss as $key => $value) {
